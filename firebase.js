99b2f1a380051243346c5245fc5766ea72b49e9c
Kenneth Ossai <kenossai@ymail.com>
2021-06-03 11:48:12 +0100

    First commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA3iH1P6OxiLbVZ9LzBiWdwQO9DapFbh2c",
+    authDomain: "tik-tok-clone-2bffd.firebaseapp.com",
+    projectId: "tik-tok-clone-2bffd",
+    storageBucket: "tik-tok-clone-2bffd.appspot.com",
+    messagingSenderId: "1038046030443",
+    appId: "1:1038046030443:web:40d9979042314ba85d5f2e",
+    measurementId: "G-R86KNMXYZQ"
+  };
